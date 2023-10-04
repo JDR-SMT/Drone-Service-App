1090e7618948834e8523ad8ab21c3a7d00bdcfe1
JDR-SMT <30058666@tafe.wa.edu.au>
2023-10-04 23:35:27 +0800

    Tasks 1-3, 5-8

---
+++ DroneServiceApp/Drone.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace DroneServiceApp
+{
+    public class Drone
+    {
+        // private variables
+        private string name;
+        private string drone;
+        private string problem;
+        private decimal cost;
+        private int tag;
+
+        #region Getters & Setters
+        public string GetName() { return name; }
+        public void SetName(string aName) { name = aName; }
+        public string GetDrone() { return drone; }
+        public void SetDrone(string aDrone) {  drone = aDrone; }
+        public string GetProblem() { return problem; }
+        public void SetProblem(string aProblem) {  problem = aProblem; }
+        public decimal GetCost() { return cost; }
+        public void SetCost(decimal aCost) {  cost = aCost; }
+        public int getTag() { return tag; }
+        public void SetTag(int aTag) { tag = aTag; }
+        #endregion
+
+        // display name and cost
+        public string Display()
+        {
+            return name + " " + cost;
+        }
+    }
+}
--- DroneServiceApp/MainWindow.xaml.cs
+++ DroneServiceApp/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DroneServiceApp
 {
@@ -24,5 +12,26 @@ namespace DroneServiceApp
         {
             InitializeComponent();
         }
+
+        // global variables
+        Queue<Drone> RegularService = new Queue<Drone>();
+        Queue<Drone> ExpressService = new Queue<Drone>();
+        List<Drone> FinishedList = new List<Drone>();
+
+        #region Utility
+
+        #endregion
+
+        #region Service Getter & Setter
+
+        #endregion
+
+        #region Add
+
+        #endregion
+
+        #region Remove
+
+        #endregion
     }
 }
